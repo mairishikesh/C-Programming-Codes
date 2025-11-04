7770014d2de8f636b9680539967845c6ec1e0ded
Rishi <rishikesh.indiraicem@gmail.com>
2025-11-04 22:10:31 +0530

    Edited C programming codes

--- password_prompt_loop.c
+++ password_prompt_loop.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 int main() {
-    char password[50];
     
-    do {
-        printf("Enter the password: ");
-        scanf("%s", password);
-    } while (strcmp(password, "secret123") != 0);
-    printf("Access granted.\n");
     
     return 0;
 }
